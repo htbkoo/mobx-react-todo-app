b37e619102c89b201ca71564fd3768f4f59bdf24
htbkoo <h1017760@connect.hku.hk>
2019-05-24 21:49:08 +0800

    built `docs/`

---
+++ docs/precache-manifest.b3d36b79c7e8958adfa592a20d82b00c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1072932261382ed6a98ba70c4fceb059",
+    "url": "/mobx-react-todo-app/index.html"
+  },
+  {
+    "revision": "89dfe06e84fbcc224491",
+    "url": "/mobx-react-todo-app/static/css/main.a8f3b025.chunk.css"
+  },
+  {
+    "revision": "2fef9732e907ea392910",
+    "url": "/mobx-react-todo-app/static/js/2.aa412297.chunk.js"
+  },
+  {
+    "revision": "89dfe06e84fbcc224491",
+    "url": "/mobx-react-todo-app/static/js/main.5962b9dd.chunk.js"
+  },
+  {
+    "revision": "b2af3db6d21b4f693048",
+    "url": "/mobx-react-todo-app/static/js/runtime~main.f992d75d.js"
+  }
+]);
